1ab2d26160d0f1aacacc00c1bf75476773070cb6
tobiasz-p <Tobiasz@DESKTOP-CK0MGLO>
2021-01-07 17:48:31 +0100

    Add JsonPrettifier class documentation

--- src/main/java/pl/put/poznan/transformer/logic/decorator/JsonPrettifier.java
+++ src/main/java/pl/put/poznan/transformer/logic/decorator/JsonPrettifier.java
@@ -1,10 +1,24 @@
 package pl.put.poznan.transformer.logic.decorator;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-
+/**
+ * JsonPrettifier class prettify output by adding whitespaces.
+ */
 public class JsonPrettifier extends JsonDecorator {
+
+    /**
+     * JsonPrettifier class constructor
+     * @param input input JSON string
+     * @throws JsonProcessingException
+     */
     public JsonPrettifier(String input) throws JsonProcessingException { super(input); }
 
+
+    /**
+     * Prettifies input JSON String
+     * @return prettified JSON as a String
+     * @throws JsonProcessingException input jsonString is invalid
+     */
     @Override
     public String transform() throws JsonProcessingException {
         return jsonNode.toPrettyString();
