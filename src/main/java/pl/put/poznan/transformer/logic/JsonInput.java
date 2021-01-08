7bbcfdb688808e85fc019256991167d3b564c91b
tobiasz-p <Tobiasz@DESKTOP-CK0MGLO>
2021-01-08 12:20:58 +0100

    Update JsonInput class documentation

--- src/main/java/pl/put/poznan/transformer/logic/JsonInput.java
+++ src/main/java/pl/put/poznan/transformer/logic/JsonInput.java
@@ -1,7 +1,13 @@
 package pl.put.poznan.transformer.logic;
 
+/**
+ * JsonInput is concrete component class
+ */
 public class JsonInput extends Json {
 
+    /**
+     * @return String "transforms"
+     */
     @Override
     public String transform() { return "transform"; }
 }
