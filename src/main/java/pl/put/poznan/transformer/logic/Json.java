163f29413d9952d2ec54ba4a81443d69a96fd9bc
Wojtek <mkwojtek@wp.pl>
2021-01-28 19:01:09 +0100

    Update documentation

--- src/main/java/pl/put/poznan/transformer/logic/Json.java
+++ src/main/java/pl/put/poznan/transformer/logic/Json.java
@@ -2,7 +2,13 @@ package pl.put.poznan.transformer.logic;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+/**
+ * Json abstract class
+ */
 public abstract class Json {
 
+    /**
+     * @throws JsonProcessingException input jsonString is invalid
+     */
     public abstract String transform() throws JsonProcessingException;
 }
