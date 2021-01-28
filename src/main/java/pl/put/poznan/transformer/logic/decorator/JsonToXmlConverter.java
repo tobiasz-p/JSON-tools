6620906eb725b1fa74b546a7a70008add8d60b22
Wojtek <mkwojtek@wp.pl>
2021-01-28 19:01:09 +0100

    Update documentation

--- src/main/java/pl/put/poznan/transformer/logic/decorator/JsonToXmlConverter.java
+++ src/main/java/pl/put/poznan/transformer/logic/decorator/JsonToXmlConverter.java
@@ -5,12 +5,25 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
+/**
+ * JsonToXmlConverter class converts XML input into JSON output
+ */
 public class JsonToXmlConverter extends JsonDecorator {
 
+    /**
+     * JsonToXmlConverter class constructor
+     * @param input input XML string
+     */
     public JsonToXmlConverter(String input) {
         super(input);
     }
 
+    /**
+     * Converts JSON input into XML output
+     * @return XML output
+     * @throws JsonProcessingException input jsonString is invalid
+     */
+    @Override
     public String transform() throws JsonProcessingException {
         try {
             ObjectMapper xmlMapper = new XmlMapper();
